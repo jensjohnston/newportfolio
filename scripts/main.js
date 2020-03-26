0c3dc2d6c85b29cc233cb0616b69847e3622ab46
jensjohnston <34891581+jensjohnston@users.noreply.github.com>
2020-03-26 23:08:50 +0100

    navigation med på sidan

--- scripts/main.js
+++ scripts/main.js
@@ -32,14 +32,15 @@ inView('section')
 
 
 
-inView('.line-1')
+inView('.lines')
     .on('enter', section => {
         // classList.add is the same as jQuery’s .addClass() method
         // but the vanilla javascript version
+        const targets = section.querySelectorAll(".line1, .line2, .line3")
         section.classList.add('in-viewport')
 
         anime({
-            targets: '.squares, .element',
+            targets: targets,
             width: '0%', // -> from '100px' to '100%',
             easing: 'easeInOutQuad',
             direction: 'normal',
@@ -58,6 +59,8 @@ inView('.line-1')
 // here we set the class to add only once we have scrolled 0.2 of 
 // our section into the viewport
 inView.threshold(0.3)
+
+
 
 
 
@@ -154,7 +157,7 @@ anime({
 
 anime({
     targets: '#el',
-    translateY: -500,
+    translateY: -300,
     direction: 'alternate',
     duration: 100000,
     loop: true,
@@ -166,3 +169,87 @@ anime({
 
 
 
+$('.menu-open').on('click', function () {
+    $('.menu').addClass('open')
+    $('body').addClass('no-scroll')
+    /* $('.top').addClass('hide')*/
+
+    return false
+})
+
+$('.menu-close').on('click', function () {
+    $('.menu').removeClass('open')
+    $('body').removeClass('no-scroll')
+    /* $('.top').removeClass('hide') */
+
+    return false
+})
+
+//smooth scroll
+
+// grab all the anchor tags on the page
+const anchors = document.querySelectorAll('a')
+// loop over them
+anchors.forEach(anchor => {
+    // listen for clicks on each one
+    anchor.addEventListener('click', event => {
+        // grab the href attribute
+        const href = anchor.getAttribute('href')
+        // if the href starts with a #
+        if (href.charAt(0) === '#') {
+            // stop the default action
+            event.preventDefault()
+            // find the element the href points to and scroll it into view
+            document.querySelector(href).scrollIntoView({
+                behavior: 'smooth'
+            })
+        }
+    })
+})
+
+
+// Hide Header on on scroll down
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('header').outerHeight();
+
+$(window).scroll(function (event) {
+    didScroll = true;
+});
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    // Make sure they scroll more than delta
+    if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight) {
+        // Scroll Down
+        $('header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+
+    lastScrollTop = st;
+}
+
+//scroll to top
+
+$("a[href='#top']").on("click", function () {
+    document.body.scrollIntoView({ block: "start", behavior: "smooth" })
+    return false
+})
