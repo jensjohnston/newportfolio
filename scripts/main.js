7cac7d5a034fafb756598108a564520a4d9753f1
jensjohnston <34891581+jensjohnston@users.noreply.github.com>
2020-03-27 18:15:46 +0100

    Experimental typography hello stranger

--- scripts/main.js
+++ scripts/main.js
@@ -63,66 +63,23 @@ inView.threshold(0.3)
 
 
 
+const wade = document.querySelector(".wade")
+
+window.addEventListener("scroll", function () {
+    const pixels = window.pageYOffset
+
+
+    const wght = 100 + pixels * 1.6
+    const wdth = 100 + pixels * 0.7
+
+    wade.style.fontVariationSettings = `"wght" ${wght}, "wdth" ${wdth}`
+    /*wade.style.fontSize = Math.min(pixels * 20, 2000) + 'px'*/
+})
 
 
 
 
 
-
-// ES6 Class
-class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-        this.txtElement = txtElement;
-        this.words = words;
-        this.txt = '';
-        this.wordIndex = 0;
-        this.wait = parseInt(wait, 10);
-        this.type();
-        this.isDeleting = false;
-    }
-
-    type() {
-        // Current index of word
-        const current = this.wordIndex % this.words.length;
-        // Get full text of current word
-        const fullTxt = this.words[current];
-
-        // Check if deleting
-        if (this.isDeleting) {
-            // Remove char
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            // Add char
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        // Insert txt into element
-        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
-
-        // Initial Type Speed
-        let typeSpeed = 100;
-
-        if (this.isDeleting) {
-            typeSpeed /= 2;
-        }
-
-        // If word is complete
-        if (!this.isDeleting && this.txt === fullTxt) {
-            // Make pause at end
-            typeSpeed = this.wait;
-            // Set delete to true
-            this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            // Move to next word
-            this.wordIndex++;
-            // Pause before start typing
-            typeSpeed = 100;
-        }
-
-        setTimeout(() => this.type(), typeSpeed);
-    }
-}
 
 
 // Init On DOM Load
