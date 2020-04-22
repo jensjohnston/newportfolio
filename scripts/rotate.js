80ad25f09ab0bc7d4229e622da36998001f6094f
jensjohnston <34891581+jensjohnston@users.noreply.github.com>
2020-04-22 23:54:26 +0200

    new code

--- scripts/rotate.js
+++ scripts/rotate.js
@@ -1,29 +1,37 @@
-let xDir = "rotateY"
-let yDir = "rotateX"
+const runRotate = () => {
+    let xDir = "rotateY"
+    let yDir = "rotateX"
 
-const sectionBook = document.querySelector("section.book")
+    const sectionBook = document.querySelector("section.book")
 
-sectionBook.addEventListener("mousemove", function (event) {
-    const x = event.pageX
-    const y = event.pageY
+    sectionBook.addEventListener("mousemove", function (event) {
+        const x = event.pageX - sectionBook.clientLeft
+        const y = event.pageY - sectionBook.clientTop
 
-    const midX = x - window.innerWidth / 2
-    const midY = y - window.innerHeight / 2
+        const midX = x - window.innerWidth / 2
+        const midY = y - window.innerHeight / 2
 
-    const box = document.querySelector("div.book")
+        const box = document.querySelector("div.book")
 
-    box.style.left = x + "px"
-    box.style.top = (sectionBook.offsetTop + y) + "px"
+        box.style.left = x + "px"
+        box.style.top = y + "px"
 
-    box.style.transform = xDir + "(" + midX + "deg) " + yDir + "(" + midY + "deg)"
-})
-
-document.querySelectorAll("select").forEach(select => {
-    select.addEventListener("change", function () {
-        if (this.name == "xDir") {
-            xDir = this.value
-        } else {
-            yDir = this.value
-        }
+        box.style.transform = xDir + "(" + midX + "deg) " + yDir + "(" + midY + "deg)"
     })
-})
+
+    document.querySelectorAll("select").forEach(select => {
+        select.addEventListener("change", function () {
+            if (this.name == "xDir") {
+                xDir = this.value
+            } else {
+                yDir = this.value
+            }
+        })
+    })
+
+}
+
+
+
+
+runRotate()
