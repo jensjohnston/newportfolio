5a4e4c44a793aa0da823a14b06ceae6ef33d8d29
jensjohnston <34891581+jensjohnston@users.noreply.github.com>
2020-04-22 22:46:53 +0200

    Book updated

--- scripts/main.js
+++ scripts/main.js
@@ -91,35 +91,7 @@ const runScripts = () => {
 
 
 
-    let xDir = "rotateY"
-    let yDir = "rotateX"
 
-    const sectionBook = document.querySelector("section.book")
-
-    sectionBook.addEventListener("mousemove", function (event) {
-        const x = event.pageX
-        const y = event.pageY
-
-        const midX = x - window.innerWidth / 2
-        const midY = y - window.innerHeight / 2
-
-        const box = document.querySelector("section.book")
-
-        box.style.left = x + "px"
-        box.style.top = y + "px"
-
-        box.style.transform = xDir + "(" + midX + "deg) " + yDir + "(" + midY + "deg)"
-    })
-
-    document.querySelectorAll("select").forEach(select => {
-        select.addEventListener("change", function () {
-            if (this.name == "xDir") {
-                xDir = this.value
-            } else {
-                yDir = this.value
-            }
-        })
-    })
 
 
     const sections = document.querySelectorAll('section')
@@ -251,7 +223,6 @@ const runScripts = () => {
         direction: 'normal',
         easing: 'easeInOutSine',
     });
-
 
 
 
---
+++ scripts/rotate.js
@@ -0,0 +1,29 @@
+let xDir = "rotateY"
+let yDir = "rotateX"
+
+const sectionBook = document.querySelector("section.book")
+
+sectionBook.addEventListener("mousemove", function (event) {
+    const x = event.pageX
+    const y = event.pageY
+
+    const midX = x - window.innerWidth / 2
+    const midY = y - window.innerHeight / 2
+
+    const box = document.querySelector("section.book")
+
+    box.style.left = x + "px"
+    box.style.top = y + "px"
+
+    box.style.transform = xDir + "(" + midX + "deg) " + yDir + "(" + midY + "deg)"
+})
+
+document.querySelectorAll("select").forEach(select => {
+    select.addEventListener("change", function () {
+        if (this.name == "xDir") {
+            xDir = this.value
+        } else {
+            yDir = this.value
+        }
+    })
+})
